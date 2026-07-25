a4a38edede06e30e0c5a86b460c7f5d73b81467d
Gideon Komla Agbavor <gideonagbavor8@gmail.com>
2026-07-25 12:42:12 +0000

    feat: implement Connect Four tutorial components and game logic

--- obj/Debug/net10.0/ConnectFour.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConnectFour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85bafce4f48e86bad5df219354a1e6396b3c0d07")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
